cbb926c3df91dc6cd6cec11912485994259974d6
David Greenberg <dsg123456789@gmail.com>
2012-05-27 19:47:49 -0400

    basic drawing commands

--- src/pcb_generator/core.clj
+++ src/pcb_generator/core.clj
@@ -1,6 +1,24 @@
-(ns pcb-generator.core)
+(ns pcb-generator.core
+  (:use [clojure.string :only [join]]))
 
-(defn -main
-  "I don't do a whole lot."
-  [& args]
-  (println "Hello, World!"))
+;Sets the wire bend to be point-to-point
+(def set-wire-bend-line
+  "set wire_bend 2")
+
+(defn set-layer
+  "Sets the layer to the given layer"
+  [layer]
+  (str "layer " layer))
+
+(defn draw-polygon
+  "Draws a polygon going through
+  the given seq of [x y] pairs.
+  The first and last pair must be the same."
+  [pairs]
+  (when-not (= (first pairs) (last pairs))
+    (throw (RuntimeException. "First and last pairs must be the same to close polygon")))
+  (str "polygon " (join \  (map (fn [[x y]]
+                                  (str \( x \  y \)))
+                                pairs))))
+
+
